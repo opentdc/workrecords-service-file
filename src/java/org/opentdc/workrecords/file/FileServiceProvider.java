2aaaa5e55ae046dddc934caa500742a16455ff68
Bruno Kaiser <bruno@bkaiser.ch>
2015-05-04 23:21:32 +0200

    import / export externalized into org.opentdc.file.AbstractFileServiceProvider

--- src/java/org/opentdc/workrecords/file/FileServiceProvider.java
+++ src/java/org/opentdc/workrecords/file/FileServiceProvider.java
@@ -23,60 +23,38 @@
  */
 package org.opentdc.workrecords.file;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletContext;
 
+import org.opentdc.file.AbstractFileServiceProvider;
 import org.opentdc.service.exception.DuplicateException;
 import org.opentdc.service.exception.NotFoundException;
 import org.opentdc.workrecords.ServiceProvider;
 import org.opentdc.workrecords.WorkRecordModel;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
-public class FileServiceProvider implements ServiceProvider {
-	
-	private static final String SEED_FN = "/seed.json";
-	private static final String DATA_FN = "/data.json";
-	private static File dataF = null;
-	private static File seedF = null;
-
-	// instance variables
-	private boolean isPersistent = true;
+public class FileServiceProvider extends AbstractFileServiceProvider<WorkRecordModel> implements ServiceProvider {
 
 	protected static Map<String, WorkRecordModel> index = new HashMap<String, WorkRecordModel>();
-
-	// instance variables
 	protected static final Logger logger = Logger.getLogger(FileServiceProvider.class.getName());
 
 	public FileServiceProvider(
 		ServletContext context, 
 		String prefix
-	) {
-		if (dataF == null) {
-			dataF = new File(context.getRealPath("/" + prefix + DATA_FN));
-		}
-		if (seedF == null) {
-			seedF = new File(context.getRealPath("/" + prefix + SEED_FN));
-		}
-		if (index.size() == 0) {
-			importJson();
+	) throws IOException {
+		super(context, prefix);
+		if (index == null) {
+			index = new HashMap<String, WorkRecordModel>();
+			List<WorkRecordModel> _workRecords = importJson();
+			for (WorkRecordModel _workRecord : _workRecords) {
+				index.put(_workRecord.getId(), _workRecord);
+			}
 		}
 	}
 
@@ -115,7 +93,7 @@ public class FileServiceProvider implements ServiceProvider {
 		_workrecord.setId(_id);
 		index.put(_workrecord.getId(), _workrecord);
 		if (isPersistent) {
-			exportJson(dataF);
+			exportJson(index.values());
 		}
 		return _workrecord;
 	}
@@ -142,7 +120,7 @@ public class FileServiceProvider implements ServiceProvider {
 			index.put(id, workrecord);
 			logger.info("updateWorkRecord(" + workrecord + ")");
 			if (isPersistent) {
-				exportJson(dataF);
+				exportJson(index.values());
 			}
 			return workrecord;
 		}
@@ -159,7 +137,7 @@ public class FileServiceProvider implements ServiceProvider {
 		index.remove(id);
 		logger.info("deleteWorkRecord(" + id + ")");
 		if (isPersistent) {
-			exportJson(dataF);
+			exportJson(index.values());
 		}
 	}
 
@@ -172,110 +150,4 @@ public class FileServiceProvider implements ServiceProvider {
 		logger.info("countWorkRecords() = " + _count);
 		return _count;
 	}
-
-	void importJson() {
-		ArrayList<WorkRecordModel> _workrecords = null;
-
-		// read the data file
-		// either read persistent data from DATA_FN
-		// or seed data from SEED_DATA_FN if no persistent data exists
-		if (dataF.exists()) {
-			logger.info("persistent data in file " + dataF.getName()
-					+ " exists.");
-			_workrecords = importJson(dataF);
-		} else { // seeding the data
-			logger.info("persistent data in file " + dataF.getName()
-					+ " is missing -> seeding from " + seedF.getName());
-			_workrecords = importJson(seedF);
-		}
-		// load the data into the local transient storage
-		for (WorkRecordModel _workrecord : _workrecords) {
-			index.put(_workrecord.getId(), _workrecord);
-		}
-		logger.info("added " + _workrecords.size() + " workrecords to index");
-
-		if (isPersistent) {
-			// create the persistent data if it did not exist
-			if (!dataF.exists()) {
-				try {
-					dataF.createNewFile();
-				} catch (IOException e) {
-					logger.severe("importJson(): IO exception when creating file "
-							+ dataF.getName());
-					e.printStackTrace();
-				}
-				exportJson(dataF);
-			}
-		}
-		logger.info("importJson(): imported " + _workrecords.size() + " workrecord objects");
-	}
-
-	/******************************** utility methods *****************************************/
-	private ArrayList<WorkRecordModel> importJson(File f) throws NotFoundException {
-		logger.info("importJson(" + f.getName() + "): importing WorkRecordsData");
-		if (!f.exists()) {
-			logger.warning("importJson(" + f.getName()
-					+ "): file does not exist.");
-			return new ArrayList<WorkRecordModel>();
-		}
-		if (!f.canRead()) {
-			logger.warning("importJson(" + f.getName()
-					+ "): file is not readable");
-			return new ArrayList<WorkRecordModel>();
-		}
-		logger.info("importJson(" + f.getName() + "): can read the file.");
-
-		Reader _reader = null;
-		ArrayList<WorkRecordModel> _workrecords = null;
-		try {
-			_reader = new InputStreamReader(new FileInputStream(f));
-			Gson _gson = new GsonBuilder().create();
-
-			Type _collectionType = new TypeToken<ArrayList<WorkRecordModel>>() {
-			}.getType();
-			_workrecords = _gson.fromJson(_reader, _collectionType);
-			logger.info("importJson(" + f.getName() + "): json data converted");
-		} catch (FileNotFoundException e1) {
-			logger.severe("importJson(" + f.getName()
-					+ "): file does not exist (2).");
-			e1.printStackTrace();
-		} finally {
-			try {
-				if (_reader != null) {
-					_reader.close();
-				}
-			} catch (IOException e) {
-				logger.severe("importJson(" + f.getName()
-						+ "): IOException when closing the reader.");
-				e.printStackTrace();
-			}
-		}
-		logger.info("importJson(" + f.getName() + "): " + _workrecords.size()
-				+ " workrecords imported.");
-		return _workrecords;
-	}
-
-	private void exportJson(File f) {
-		logger.info("exportJson(" + f.getName() + "): exporting workrecords");
-		Writer _writer = null;
-		try {
-			_writer = new OutputStreamWriter(new FileOutputStream(f));
-			Gson _gson = new GsonBuilder().create();
-			_gson.toJson(index.values(), _writer);
-		} catch (FileNotFoundException e) {
-			logger.severe("exportJson(" + f.getName() + "): file not found.");
-			e.printStackTrace();
-		} finally {
-			if (_writer != null) {
-				try {
-					_writer.close();
-				} catch (IOException e) {
-					logger.severe("exportJson(" + f.getName()
-							+ "): IOException when closing the reader.");
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
 }
